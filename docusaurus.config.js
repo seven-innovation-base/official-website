2385dc4e0fe0b6fa26fa2cf61096ad16d3454321
yeshan333 <1329441308@qq.com>
2020-05-30 15:21:09 +0800

    fix(site): fix edit url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -110,12 +110,12 @@ module.exports = {
           homePageId: 'intro',
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/seven-innovation-base/official-website',
+          editUrl: 'https://github.com/seven-innovation-base/official-website/tree/master',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
-          editUrl: 'https://github.com/seven-innovation-base/official-website',
+          editUrl: 'https://github.com/seven-innovation-base/official-website/tree/master',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
