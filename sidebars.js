e1cff00beaed103584969259f87b3785a52c4bc2
yeshan333 <1329441308@qq.com>
2020-05-31 13:27:49 +0800

    fix: fix ssl, remove CNAME file

--- sidebars.js
+++ sidebars.js
@@ -1,7 +1,11 @@
 module.exports = {
   docs: {
     创新基地简介: ['intro'],
-    公告: ['contest/contest-02', 'notice/holiday-practice-1'],
+    公告: [
+      'contest/contest-02',
+      'notice/holiday-practice-1',
+      'notice/2019-2020-annual-review',
+    ],
     基地各部门简介: [
       'introduction/Algorithms',
       'introduction/Software',
