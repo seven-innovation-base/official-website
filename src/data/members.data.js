76e8bf19bde3c1dfed9ba85baca1d97690d545d4
SuZe <58203118+suze233@users.noreply.github.com>
2021-02-28 22:09:22 +0800

    Update members.data.js (#21)
    * Update members.data.js
    
    * Update members.data.js
    
    Co-authored-by: yeshan333 <1329441308@qq.com>

--- src/data/members.data.js
+++ src/data/members.data.js
@@ -15,7 +15,7 @@ const members = [
     blogUrl: 'https://yww52.com/',
   },
   {
-    name: 'yeshan',
+    name: 'yeshan333',
     avatar: 'https://github.com/yeshan333.png',
     description: '玄学爱好者&摸🐟大师，资深混子',
     githubUrl: 'https://github.com/yeshan333',
@@ -28,14 +28,14 @@ const members = [
     githubUrl: 'https://github.com/MrGo123',
     blogUrl: 'https://zy68.top/'
   },
-    {
+  {
     name: 'redhat',
     avatar: 'https://avatars.githubusercontent.com/u/57751257?s=460&u=634fb3d9085fd46aead74e7d0db0ae3d1d933f3f&v=4',
     description: '题不可一日不刷',
     githubUrl: 'https://github.com/redhat123456',
     blogUrl: 'https://redhat123456.github.io/'
   },
-   {
+  {
     name: 'Higgins',
     avatar: 'https://i.loli.net/2020/07/31/QLmPF47aCS8sYGf.jpg',
     description: '道生一，一生二',
@@ -50,6 +50,13 @@ const members = [
     blogUrl: 'http://www.lvshaomei.club/',
   },
   {
+    name: 'szue',
+    avatar: 'https://picture.suze666.top/images/2020/02/20/22da933f1a91d0a482256d0a88440337.jpg',
+    description: '今天也是努力写代码的一天呢',
+    githubUrl: 'https://github.com/suze233',
+    blogUrl: 'https://suze666.top'
+  },
+  {
     name: 'lei',
     avatar: 'http://github.com/Leishen-hub.jpeg',
     description: '鱼',
@@ -61,8 +68,8 @@ const members = [
     avatar: 'https://avatars.githubusercontent.com/u/67368157?s=460&v=4',
     description: '从菜鸟到大神',
     githubUrl: 'https://github.com/010505',
-    blogUrl: 'https://github.com/010505/official-website',
-  }
+    blogUrl: 'https://github.com/010505/official-website'
+  },
 ];
 
 export default members;
