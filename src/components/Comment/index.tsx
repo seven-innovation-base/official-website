21d31010cdb1c2ab1b474a4643cbc94bfbb46733
redhat123456 <1907065810@qq.com>
2022-08-04 09:33:21 +0800

    update

--- src/components/Comment/index.tsx
+++ src/components/Comment/index.tsx
@@ -48,7 +48,7 @@ export default function App() {
           />
         </li>
       )}
-      /> : <Spin tip="Loading...">
+      /> : <Spin tip="Loading..." size="large">
     </Spin>} 
     </div> }
     
