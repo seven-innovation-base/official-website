895a618f3fe054cd9d3df8d82db606eb3eee4169
redhat123456 <1907065810@qq.com>
2022-09-28 22:40:14 +0800

    add jiaruwe (#94)

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,3 +1,8 @@
 export class CommentAPI {
   static GithubIssueUrl: string = "https://api.github.com/repositories/267360943/issues/86/comments"
+
+}
+
+export class AxieyunAPI {
+  static AxieyunUrl: string = "https://tanger.fit:7777"
 }
