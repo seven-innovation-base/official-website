820be2cdfc24407571f4da18adea4c6f0b75b164
羊村的风吹不到比奇堡，我和她也没有明天 <95482898+zqqqqqqj1110@users.noreply.github.com>
2023-11-05 16:53:45 +0800

    Update members.data.ts
    博客地址改变

--- src/data/members.data.ts
+++ src/data/members.data.ts
@@ -138,7 +138,7 @@ const members: Member[] = [
     avatar: 'github:zqqqqqqj1110',
     description: '知行合一',
     githubUrl: 'https://github.com/zqqqqqqj1110',
-    blogUrl: 'http://47.113.196.76/',
+    blogUrl: 'http://8.130.141.48/',
   },
   {
     name: '情用香烟麻醉', 
