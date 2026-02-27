60c0ae6293accc26ebcb5d565bc951e7d2b4d90e
redhat123456 <1907065810@qq.com>
2026-02-27 11:16:48 +0800

    fix: clean deploy config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -7,7 +7,7 @@ module.exports = {
   favicon: 'img/logo.png',
   organizationName: 'seven-innovation-base', // Usually your GitHub org/user name.
   projectName: 'seven-innovation-base.github.io', // Usually your repo name.
-  deploymentBranch: 'master',
+  deploymentBranch: 'main',
   trailingSlash: true,
   themeConfig: {
     navbar: {
